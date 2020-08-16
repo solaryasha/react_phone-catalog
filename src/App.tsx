74d6fd6a093a5099212c6dce426747a80c65018c
solaryasha <solaryasha@gmail.com>
2020-08-16 14:55:08 +0300

    Initial project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <div className="App">
-    <h1>React Phone Catalog</h1>
+    <Navigation />
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/phones" component={Phones} />
+      <Route path="/tablets" component={Tablets} />
+      <Route path="/accessories" component={Accessories} />
+      <Route path="/favorites" component={Favorites} />
+    </Switch>
   </div>
 );
 
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+export const Navigation = () => {
+  return (
+    <div>
+      <Logo />
+      <Nav />
+      <Favorites />
+      <Cart />
+    </div>
+  );
+};
